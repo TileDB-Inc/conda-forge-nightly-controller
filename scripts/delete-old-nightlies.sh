db9b349d76ffa69cc617e36d4794d9b86a0751e9
John Blischak <jdblischak@gmail.com>
2023-03-24 14:18:49 -0400

    Prepend TILEDB_CI_ to ensure env vars are unique

--- scripts/delete-old-nightlies.sh
+++ scripts/delete-old-nightlies.sh
@@ -4,36 +4,36 @@ set -euo pipefail
 #
 # Usage:
 #   export ANACONDA_TOKEN="<api-token>"
-#   ACCOUNT=tiledb DAYS=7 scripts/delete-old-nightlies.sh
+#   TILEDB_CI_ACCOUNT=tiledb TILEDB_CI_DAYS=7 scripts/delete-old-nightlies.sh
 #
 # Variables that must be defined outside of this script:
 #
 # ANACONDA_TOKEN: (string) API token for anaconda.org
-# ACCOUNT: (string) The main account/channel on anaconda.org. The actual channel
-#          to be searched will be "ACCOUNT/label/nightlies"
-# DAYS: (integer) Number of days to keep nightlies (ie nightlies uploaded more
-#       than $DAYS days ago will be removed)
+# TILEDB_CI_ACCOUNT: (string) The main account/channel on anaconda.org. The actual channel
+#          to be searched will be "TILEDB_CI_ACCOUNT/label/nightlies"
+# TILEDB_CI_DAYS: (integer) Number of days to keep nightlies (ie nightlies uploaded more
+#       than $TILEDB_CI_DAYS days ago will be removed)
 #
 # Variables that can be defined outside of this script:
 #
-# SUBDIR: linux-64 (default), linux-aarch64, linux-ppc64le, osx-64, osx-arm64, win-64
-# DRY_RUN: (integer) Exit without actually removing packages. Default is 0. Set
+# TILEDB_CI_SUBDIR: linux-64 (default), linux-aarch64, linux-ppc64le, osx-64, osx-arm64, win-64
+# TILEDB_CI_DRY_RUN: (integer) Exit without actually removing packages. Default is 0. Set
 #          to 1 for dry run
 
 # optional variables
-SUBDIR="${SUBDIR:-linux-64}"
-DRY_RUN="${DRY_RUN:-0}"
+TILEDB_CI_SUBDIR="${TILEDB_CI_SUBDIR:-linux-64}"
+TILEDB_CI_DRY_RUN="${TILEDB_CI_DRY_RUN:-0}"
 
-channel="$ACCOUNT/label/nightlies"
-echo "Removing $SUBDIR nightlies more than $DAYS days old from the channel $channel"
+channel="$TILEDB_CI_ACCOUNT/label/nightlies"
+echo "Removing $TILEDB_CI_SUBDIR nightlies more than $TILEDB_CI_DAYS days old from the channel $channel"
 
-conda search -v --json --override-channels -c "$channel" --subdir "$SUBDIR" > nightlies.json
+conda search -v --json --override-channels -c "$channel" --subdir "$TILEDB_CI_SUBDIR" > nightlies.json
 
 total=$(jq 'flatten | length' nightlies.json)
 echo "Total nightlies: $total"
 
 # in milliseconds since the epoch
-cutoff=$(expr $(date --date="$DAYS days ago" +%s) \* 1000)
+cutoff=$(expr $(date --date="$TILEDB_CI_DAYS days ago" +%s) \* 1000)
 
 jq -r --argjson cutoff $cutoff \
   'flatten | .[] | select(.timestamp < $cutoff) | .name + "/" + .version + "/" + .subdir + "/" + .fn' \
@@ -42,7 +42,7 @@ jq -r --argjson cutoff $cutoff \
 total_to_be_removed=$(cat nightlies-to-remove.txt | wc -l)
 echo "Total to be removed: $total_to_be_removed"
 
-if [[ "$DRY_RUN" -eq 1 ]]
+if [[ "$TILEDB_CI_DRY_RUN" -eq 1 ]]
 then
   echo "Dry run: exit early"
   exit 0
@@ -52,7 +52,7 @@ fi
 
 for nightly in `cat nightlies-to-remove.txt`
 do
-  echo "$ACCOUNT/$nightly"
-  anaconda --token "$ANACONDA_TOKEN" remove --force "$ACCOUNT/$nightly"
+  echo "$TILEDB_CI_ACCOUNT/$nightly"
+  anaconda --token "$ANACONDA_TOKEN" remove --force "$TILEDB_CI_ACCOUNT/$nightly"
   sleep 0.1
 done
