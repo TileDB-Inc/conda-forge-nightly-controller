e0f2ca0c2d631a29751f6a9d643fedf8b6f81216
John Blischak <jdblischak@gmail.com>
2024-04-16 11:42:30 -0400

    Remove patches for 0.22.0 now that it has been released

--- scripts/tiledb/update-recipe.sh
+++ scripts/tiledb/update-recipe.sh
@@ -23,27 +23,5 @@ sed -i \
   s/"  number: [0-9]\+"/"  number: 0"/ \
   tiledb-feedstock/recipe/meta.yaml
 
-# (temporary) Add azure-identity-cpp
-mkdir -p tiledb-feedstock/recipe/tiledb-patches/system-ports/azure-identity-cpp
-echo "set(VCPKG_POLICY_EMPTY_PACKAGE enabled)" \
-  > tiledb-feedstock/recipe/tiledb-patches/system-ports/azure-identity-cpp/portfile.cmake
-echo '{ "name": "azure-identity-cpp", "version-string": "system" }' \
-  > tiledb-feedstock/recipe/tiledb-patches/system-ports/azure-identity-cpp/vcpkg.json
-
-sed -i \
-  s/host:/'host:\n    - azure-identity-cpp'/ \
-  tiledb-feedstock/recipe/meta.yaml
-
-# Use VS2022
-cat >> tiledb-feedstock/recipe/conda_build_config.yaml << EOF
-c_compiler:    # [win]
-  - vs2022       # [win]
-cxx_compiler:  # [win]
-  - vs2022       # [win]
-c_stdlib_version:    # [win]
-  - 2022             # [win]
-EOF
-
-
 # Print differences
 git -C tiledb-feedstock/ --no-pager diff recipe/meta.yaml
