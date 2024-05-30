1699ab4dc7512adcf336d38cf63a297acb6685a1
John Blischak <jdblischak@gmail.com>
2024-05-30 10:19:22 -0400

    Fix PR 99

--- scripts/add-and-commit.sh
+++ scripts/add-and-commit.sh
@@ -8,4 +8,4 @@ git -C "$repo" config --local user.name "GitHub Actions"
 git -C "$repo" config --local user.email "runneradmin@github.com"
 git -C "$repo" checkout --quiet -B nightly-build main
 git -C "$repo" add .
-git -C "$repo" commit --quiet -m "Nightly build for $version"
+git -C "$repo" commit -m "Nightly build for $version"
