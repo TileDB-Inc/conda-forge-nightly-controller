4fbc77c68c69c151b0d974ccb132f380bc14796e
Agisilaos Kounelis <36283973+kounelisagis@users.noreply.github.com>
2024-11-22 16:35:40 +0200

    Never remove deprecations

--- scripts/tiledb-py/update-recipe.py
+++ scripts/tiledb-py/update-recipe.py
@@ -64,8 +64,8 @@ for i in range(len(updated["requirements"]["host"])):
 with open(recipe, "w") as f:
     yaml.dump(updated, f)
 
-# Run with deprecation warnings on Mondays for forward-looking alerts.
-remove_deprecations_value = "ON" if datetime.today().weekday() == 0 else "OFF"
+# Run without deprecation warnings
+remove_deprecations_value = "OFF"
 
 # Create OS-specific build scripts
 with open("tiledb-py-feedstock/recipe/build.sh", "w") as f:
